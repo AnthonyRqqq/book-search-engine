27f8ed5f42ebb7c27669addd0a3d581f83262b77
unknown <anthony_rosati@yahoo.com>
2024-03-01 23:33:55 -0600

    Updated code in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,25 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+// Enables interaction with GraphQL
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 import Navbar from './components/Navbar';
 
+// Creates instance of graphql endpoint
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache()
+});
+
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    // ApolloProvider wrapper enables access to ApolloClient from anywhere in program
+    <ApolloProvider client={client}>
+      <>
+        <Navbar />
+        <Outlet />
+      </>
+    </ApolloProvider>
   );
 }
 
