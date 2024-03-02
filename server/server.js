8ef8e452c62fd0268fceae5870dc3743e3e2ad7c
unknown <anthony_rosati@yahoo.com>
2024-03-02 00:49:02 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -20,6 +20,7 @@ const startApolloServer = async () => {
 
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
+  app.use(routes);
 
   app.use('/graphql', expressMiddleware(server, {
     context: authMiddleware
