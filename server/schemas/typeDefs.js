6306ded61e77344706468127bcd8ee93e4361535
unknown <anthony_rosati@yahoo.com>
2024-03-02 22:29:04 -0600

    Bugfix

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,14 +1,14 @@
 const typeDefs = `
-    // Type for User
+    # Type for User
     type User {
         _id: ID
         username: String
         email: String
-        bookCount: String
+        bookCount: Int
         savedBooks: [Book]
     }
 
-    // Type for Book
+    # Type for Book
     type Book {
         authors: [String]
         description: String
@@ -18,29 +18,39 @@ const typeDefs = `
         title: String
     }
 
-    // Type for Auth
+    # Type for Auth
     type Auth {
         token: String
         user: User
     }
 
-    // Query for user accessing the program
+    # BookInput type
+    input BookInput {
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
+
+    # Query for user accessing the program
     type Query {
         me(userId: ID!): User
     }
 
     type Mutation {
-        // For login, returns an Auth type
+        # For login, returns an Auth type
         login(email: String!, password: String!): Auth
 
-        // For creating a user, returns an Auth type
+        # For creating a user, returns an Auth type
         addUser(username: String!, email: String!, password: String!): Auth
 
-        // For saving a book from an input based on the Book type, returns a User type
-        saveBook(input: Book!, userId: ID!): User
+        # For saving a book from an input based on the Book type, returns a User type
+        saveBook(input: BookInput!, userId: ID!): User
 
-        // For removing a book based on the bookId, returns a User type
-        removeBook(bookId: bookId!, userId: ID!): User
+        # For removing a book based on the bookId, returns a User type
+        removeBook(bookId: String!, userId: ID!): User
     }
 `
 
