5891a451682c842487986741f8b2f9d71ccd8062
unknown <anthony_rosati@yahoo.com>
2024-03-02 22:38:15 -0600

    Updated code in auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -10,6 +10,32 @@ module.exports = {
       code: 'UNAUTHENTICATED',
     },
   }),
+
+  authMiddleware: function (req, res, next) {
+    // allows token to be sent via  req.query or headers
+    let token = req.query.token || req.headers.authorization;
+
+    // ["Bearer", "<tokenvalue>"]
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      this.AuthenticationError;
+    }
+
+    // verify token and get user data out of it
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+      return this.AuthenticationError;
+    }
+
+    // send to next endpoint
+    next();
+  },
   signToken: function ({ email, name, _id }) {
     const payload = { email, name, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
